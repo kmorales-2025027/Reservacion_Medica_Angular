9825f91faaa55e890d5072e0ac086b8b992c486a
Roxydevl <jmartinez-2025414@kinal.edu.gt>
2026-09-01 07:53:27 -0600

    chore: inicializar la estructura del proyecto

---
+++ Reservacion_Medica_Angular/src/app/components/form/form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-form',
+  styleUrl: './form.css',
+  templateUrl: './form.html',
+})
+export class Form {}
---
+++ Reservacion_Medica_Angular/src/app/components/listado/listado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-listado',
+  styleUrl: './listado.css',
+  templateUrl: './listado.html',
+})
+export class Listado {}
